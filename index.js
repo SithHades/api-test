89b9ad66275a02acfb20e8f0f42fa2db560fc059
Luca B. Knaack <l.knaack93@gmail.com>
2023-01-25 00:38:56 +0100

    added initial console output to module

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express()
 
+const port = 3000;
+const hostname = 'localhost';
+
 app.get('/api/test', (req, res) => {
     const apiKey = req.query.apiKey;
 
     res.send({data: 'API endpoint exposed.'})
 })
 
-app.listen(8080)
+app.listen(port, hostname, () => {
+    console.log('Server is running at http://${hostname}:${port}/');
+})
